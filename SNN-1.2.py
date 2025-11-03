7cad8ddbe3be5862fd233cb470c1c131099b179a
GeoMagnus <54456125+MagnusHjornhede@users.noreply.github.com>
2025-11-03 12:28:48 +0100

    Add files via upload

---
+++ SNN-1.2.py
@@ -0,0 +1,74 @@
+# ***********************
+# Code for Task 2
+# ***********************
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+
+def rasterplot(ax, x, y, x_label, y_label):
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    ax.scatter(x, y, marker='|')
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+
+if __name__ == '__main__':
+    # plt.rcParams['figure.figsize'] = [10, 10]
+    # Parameters for Tasks 1–5
+
+    # Timestep
+    dt = 0.00001
+
+    # Neuron parameters
+    R = 95e6  # resistance in ohms
+    tau_m = 3e-3  # Membrane constant in seconds
+    u_rest = -65e-3  # resting membrane potential in V
+    u_reset = -65e-3  # reset potential in V
+    u_thres = -50e-3  # threshold potential in V
+
+    # Code for Task 1
+
+    # Input current from synapses
+    I_syn = 50e-12  # A
+
+    # Neuron identifier, only one neuron in this simulation
+    n_id = 0
+
+    # Placeholders for u(t), used for plotting
+    t_i = []
+    u_i = []
+
+    # Placeholders for list of spike times and neuron ID's
+    t_spike = []
+    n_spike = []
+
+
+I_syn = 50  # pA
+# Re-initializing the spiking detection setup
+found_spike = False  # Reset the flag for detecting spikes
+spiking_current = None  # Reset the spiking current value
+print("Start")
+# Corrected loop to find the spiking current
+for I_syn in range(50, 1000, 2):  # from 50 pA up to 1000 pA
+    t = 0  # Reset time for each I_syn value
+    u = u_rest  # Reset membrane potential for each simulation
+
+    while t <= 1:
+        # Euler forward method for du/dt
+        du_dt = (-(u - u_rest) + R * (I_syn * 1e-12)) / tau_m
+        u += du_dt * dt
+
+
+        if u >= u_thres:  # Check for spike
+            spiking_current = I_syn  # Store the spiking current value
+            found_spike = True  # Set the flag to true
+            break  # Exit the inner loop if a spike is found
+
+        t += dt  # Increment time
+
+    if found_spike:
+        print(f"Found spike at: {spiking_current}pA")
+
+        break  # Exit the outer loop if a spike has been found
+else:
+    print("No spiking current found within the tested range.")
+# Plotting
---
+++ task_1.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+
+def load_data(filepath: str) -> pd.DataFrame:
+
+    feature_names = [f"feature_{i}" for i in range(1, 243)]  # 242 feature columns
+    data = pd.read_csv(filepath, header=None, names=feature_names)
+    data.rename(columns={
+        data.columns[-2]: 'gesture name',
+        data.columns[-1]: 'gesture ID'
+    }, inplace=True)
+    return data
+
+
+def check_missing_values(_input: pd.DataFrame):
+    missing_values = _input.isnull().sum()
+    columns_with_missing_values = missing_values[missing_values > 0]
+    print("Missing values")
+    print(columns_with_missing_values)
+
+
+def print_heatmap(_input: pd.DataFrame, _name: str):
+    plt.figure(figsize=(12, 5))
+    sns.heatmap(_input.isnull(), cmap='viridis', cbar=True, yticklabels=False)
+    plt.title(_name)
+    plt.show()
+
+
+def plot_boxplot(_input: pd.DataFrame, _name: str):
+    _input.boxplot(figsize=(12, 8))
+    plt.xticks(rotation=90)  # Rotate x labels
+    plt.title(_name)
+    plt.show()
+
+
+if __name__ == '__main__':
+    # Load data
+    train_data = load_data('datasets/train-final.csv')
+    test_data = load_data('datasets/test-final.csv')
+
+    # convert into dataFrame
+    train_data = pd.DataFrame(train_data)
+    test_data = pd.DataFrame(test_data)
+
+    # Check for missing values
+    print("Checking train_data")
+    check_missing_values(train_data)
+    print_heatmap(train_data, "Missing data points in train-final.csv")
+    print("Check test_data")
+    check_missing_values(test_data)
+    print_heatmap(test_data, "Missing data points in test-final.csv")
+
+    # Assign data for training
+    X = train_data.drop(columns=['gesture name', 'gesture ID'])
+    y = train_data['gesture ID']  # gesture ID as the target label
+    X.fillna(X.mean(), inplace=True)
+    # X.dropna(inplace=True)
+
+    # Assign data for testing
+    test_data.dropna(inplace=True)  # Just drop rows missing testdata
+    X_test = test_data.drop(columns=['gesture name', 'gesture ID'])
+    y_test = test_data['gesture ID']  # gesture ID as  target label
+    X_test.fillna(X.mean(), inplace=True)
+
+    positions_data = X.iloc[:, :60]  # first 60 columns
+    cosine_angles_data = X.iloc[:, 60:120]  # next 60 columns
+    mean_positions_data = X.iloc[:, 120:180]  # next 60 columns
+    std_positions_data = X.iloc[:, 180:240]  # next 60 columns
+
+    # Print out a plot
+    #plot_boxplot(positions_data, 'positions data')
+    #plot_boxplot(cosine_angles_data, 'cosine angles data')
+    #plot_boxplot(mean_positions_data, 'mean positions data')
+    #plot_boxplot(std_positions_data, 'std positions data')
+
+    # Standardlization
+    first_60_features = train_data.iloc[:, :60]
+    scaler = MinMaxScaler()
+    normalized_first_60_features = scaler.fit_transform(first_60_features)
+    #scaler = StandardScaler()
+    #normalized_first_60_features = scaler.fit_transform(first_60_features)
+    train_data.iloc[:, :60] = normalized_first_60_features
+
+    plot_boxplot(train_data, "First 60 standardization")
